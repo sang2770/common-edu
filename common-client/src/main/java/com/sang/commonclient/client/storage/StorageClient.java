6eb00f2df0d618b0e3617fa9b11809bc9dfcd779
sang2770 <nguyenvansang30102001@gmail.com>
2023-01-31 21:57:33 +0700

    fix bug client

--- common-client/src/main/java/com/sang/commonclient/client/storage/StorageClient.java
+++ common-client/src/main/java/com/sang/commonclient/client/storage/StorageClient.java
@@ -4,6 +4,7 @@ import com.sang.commonclient.domain.FileDTO;
 import com.sang.commonmodel.dto.response.Response;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +13,9 @@ import org.springframework.web.multipart.MultipartFile;
 @LoadBalancerClient(name = "storage")
 @FeignClient(name = "storage", fallbackFactory = StorageClientFallBack.class )
 public interface StorageClient {
-    @PostMapping("/storages/upload-firebase")
+    @PostMapping("/api/storages/upload-firebase")
     Response<FileDTO> uploadFileBase(@RequestParam("file") MultipartFile file) throws Exception;
 
-    @PostMapping("/storages/find-by-id/{id}")
+    @GetMapping("/api/storages/find-by-id/{id}")
     Response<FileDTO> findById(@PathVariable String id);
 }
